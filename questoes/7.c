7b5ba4d489c71e1062cdbe55dfa76079e28c3ca6
Renata <127506139+rcrenata@users.noreply.github.com>
2024-07-30 17:53:42 -0400

    7.c

--- questoes/7.c
+++ questoes/7.c
@@ -5,12 +5,16 @@ int main() {
     int *pvet;
     int n;
     pvet = (int*) malloc(n*sizeof(int));
+    printf("Tamanho da lista: ");
     scanf("%d", &n);
     for(int i=0; i<n; i++){
+        printf("%d valor: ", i);
         scanf("%d", &pvet[i]);
     }
+
+    printf("Lista na ordem inversa: ");
     for(int i=(n-1); i>=0; i--){
-        printf("%d\n", pvet[i]);
+        printf("%d ", pvet[i]);
     }
     free(pvet);
     
